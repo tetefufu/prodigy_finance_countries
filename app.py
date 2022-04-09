8178866e5024cce29dfb5bc79f25eee626340bdf
Joel <joelbellot@hotmail.com>
2022-04-09 16:27:20 +0000

    launch, country by currency

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restful import Resource, Api, fields, marshal_with
+from flask import Flask, request
+from flask_restful import Resource, Api, fields, marshal_with, reqparse
 import jsonify
 
 app = Flask(__name__)
@@ -27,6 +27,7 @@ class CountryDao:
 
 COUNTRIES = [
     CountryDao("United States of America", "US", "USA", ["USD"]),
+    CountryDao("United Kingdom", "GB", "GBR", ["GBP"]),
     CountryDao("Ukraine", "UA", "UKR", ["UAH"]),
     CountryDao("Brunei", "BN", "BRN", ["BND", "SGD"]),
 ]
@@ -35,7 +36,10 @@ COUNTRIES = [
 class CountriesResource(Resource):
     @marshal_with(country_dao_fields)
     def get(self):
-        return COUNTRIES
+        currency = request.args.get("currency", default=None, type=str)
+        if currency == None:
+            return COUNTRIES
+        return [x for x in COUNTRIES if currency in x.currencies]
 
 
 class CountryResource(Resource):
--- test_app.py
+++ test_app.py
@@ -6,7 +6,19 @@ def test_get_all_countries():
     with app.test_client() as test_client:
         response = test_client.get("/countries")
         res = json.loads(response.data.decode("utf-8"))
-        assert len(res) == 3
+        assert len(res) == 4
+        usa = res[0]
+        assert usa["name"] == "United States of America"
+        assert usa["alpha_2_code"] == "US"
+        assert usa["alpha_3_code"] == "USA"
+        assert usa["currencies"][0] == "USD"
+
+
+def test_get_country_given_currency():
+    with app.test_client() as test_client:
+        response = test_client.get("/countries?currency=USD")
+        res = json.loads(response.data.decode("utf-8"))
+        assert len(res) == 1
         usa = res[0]
         assert usa["name"] == "United States of America"
         assert usa["alpha_2_code"] == "US"
